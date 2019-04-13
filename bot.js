6f2ef6bb3935c68e68df8b244bfd57df84ddf450
kaandeveci34 <49440153+kaandeveci34@users.noreply.github.com>
2019-04-13 14:21:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,9 +70,7 @@ client.on('message', async msg => {
 		if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
 			const playlist = await youtube.getPlaylist(url);
 			const videos = await playlist.getVideos();
-			for (const video of Object.values(videos)) {
-				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
-				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
+			for (const video of Object.values(videos))
 			}
 			 return msg.channel.sendEmbed(new Discord.RichEmbed)
       .setTitle(`**✅ | Oynatma Listesi: **${playlist.title}** Kuyruğa Eklendi!**`)
